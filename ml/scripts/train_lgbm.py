bca10d7e49729e472b0740eaf54803b2fdd54dca
Pannapatt Khumwiwat <pkm@asleep.local>
2026-07-12 11:20:35 +0700

    lgbm weather prediction and risk scoring

--- ml/scripts/train_lgbm.py
+++ ml/scripts/train_lgbm.py
@@ -16,11 +16,13 @@ Run: python ml/scripts/train_lgbm.py
 
 import pandas as pd
 import numpy as np
+import json
 from pathlib import Path
 from lightgbm import LGBMClassifier
 from sklearn.metrics import f1_score
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
+from lightgbm import early_stopping
 
 
 def train(processed_path: Path, model_output_path: Path):
@@ -28,57 +30,99 @@ def train(processed_path: Path, model_output_path: Path):
     df = df.sort_values("date")
 
     # Split (80/20) 
-    split = int(len(df) * 0.8)
-    train_df = df.iloc[:split].copy()
-    test_df = df.iloc[split:].copy()
+    train_end = int(len(df) * 0.64)
+    val_end = int(len(df) * 0.80)
 
-    # Calculate threshold
-    threshold = train_df["rain"].quantile(0.95) # only compute on train
-    train_df["flash_storm"] = (train_df["rain"] > threshold).astype(int)
-    test_df["flash_storm"] = (test_df["rain"] > threshold).astype(int)
+    train_df = df.iloc[:train_end].copy()
+    val_df = df.iloc[train_end:val_end].copy()
+    test_df = df.iloc[val_end:].copy()
 
-    X_train = train_df.drop(columns=["flash_storm", "rain", "date"])
-    y_train = train_df["flash_storm"]
-    X_test = test_df.drop(columns=["flash_storm", "rain", "date"])
-    y_test = test_df["flash_storm"]
+    selected_features = [
+        "day_sin",
+        "day_cos",
+
+        "rain",
+        "temp",
+        "temp_max",
+        "temp_min",
+
+        "humidity",
+        "pressure",
+        "wind",
+        "wind_dir",
+        "radiation"
+    ]
+
+    X_train =  train_df[selected_features]
+    y_train = train_df["heavy_rain_day1"]
+
+    X_val =  val_df[selected_features]
+    y_val = val_df["heavy_rain_day1"]
+
+    X_test =  test_df[selected_features]
+    y_test = test_df["heavy_rain_day1"]
 
     # LGBM model
     model = LGBMClassifier(
-      n_estimators=1000, # Number of decision trees
-      learning_rate=0.03, # How much each new tree changes the model
-      num_leaves=15,
-      max_depth=6,
+      n_estimators=1500, # Number of decision trees
+      learning_rate=0.02, # How much each new tree changes the model
+      num_leaves=31,
+      max_depth=-1,
       class_weight="balanced", # Tell the classifier to pay more attention to y value
       random_state=42,
-      feature_fraction=0.7, # Each tree is trained using (no.) of the available features
-      bagging_fraction=0.8, # Each tree is trained using (no.) of trainig samples
-      bagging_freq=5, # Creates a new random sample every (no.) trees.
-      min_child_samples=20 # Sets the minimum number of training samples required in a leaf
+      colsample_bytree=0.8, # Each tree is trained using (no.) of the available features
+      subsample=0.8, # Each tree is trained using (no.) of trainig samples
+      subsample_freq=5, # Creates a new random sample every (no.) trees.
+      reg_alpha=0.5,
+      reg_lambda=1.0,
     )
 
     # Train model
-    model.fit(X_train, 
-              y_train, 
-              eval_set=(X_test, y_test)
+    model.fit(
+        X_train,
+        y_train,
+        eval_set=[(X_val, y_val)],
+        callbacks=[
+            early_stopping(100)
+        ]
     )
 
     # Predict test data
-    y_prob = model.predict_proba(X_test)[:, 1]
+    y_prob_val = model.predict_proba(X_val)[:, 1]
 
-    # Find the best threshold that gives the highest f1 score
     best_f1 = 0
     best_threshold = 0
-    for threshold in np.arange(0.05, 0.55, 0.05):
-        y_pred = (y_prob >= threshold).astype(int)
-        f1 = f1_score(y_test, y_pred) # Calculate f1 score
-        if f1 > best_f1:
-            best_f1 = f1
-            best_threshold = threshold
-    print(f"Best Threshold: {best_threshold}")
-    print(f"Best F1: {best_f1}")
 
-    # Use the best threshold
-    y_pred = (y_prob >= best_threshold).astype(int)
+    # Find best threshold
+    # for threshold in np.arange(0.05, 0.95, 0.05):
+    #     y_pred = (y_prob_val >= threshold).astype(int)
+
+    #     f1 = f1_score(y_val, y_pred)
+
+    #     if f1 > best_f1:
+    #         best_f1 = f1
+    #         best_threshold = threshold
+
+    # print(f"Best Threshold: {best_threshold:.2f}")
+    # print(f"Validation F1: {best_f1:.3f}")
+
+    # Fixed threshold
+    best_threshold = 0.15
+
+    # Save best threshold
+    config = {
+        "threshold": float(best_threshold)
+    }
+    with open(model_output_path.parent / "model_config.json", "w") as f:
+        json.dump(config, f, indent=4)
+
+    # Predict on the test set
+    y_prob_test = model.predict_proba(X_test)[:, 1]
+
+    y_pred = (y_prob_test >= best_threshold).astype(int)
+
+    f1 = f1_score(y_test, y_pred)
+    print(f"F1 score: {f1}")
 
     print(confusion_matrix(y_test, y_pred))
     # Results : [[TN(True neg) FP(False pos)], [FN(False neg) TP(True pos)]]
@@ -95,7 +139,7 @@ def train(processed_path: Path, model_output_path: Path):
       "Importance": model.feature_importances_
     }).sort_values(by="Importance", ascending=False)
     
-    # print(importance.tail(10))
+    print(importance.head(20))
 
     # Save importance features
     importance.to_csv(
@@ -107,11 +151,16 @@ def train(processed_path: Path, model_output_path: Path):
     model.booster_.save_model(model_output_path)    
     print(f"Model saved to {model_output_path}")
 
+    # Save features
+    with open(model_output_path.parent / "feature_names.json", "w") as f:
+        json.dump(selected_features, f, indent=4)
+    print("Features saved to feature_names.json")
+
 
 if __name__ == "__main__":
     base = Path(__file__).resolve().parents[1]
 
     processed_path = base / "data" / "processed" / "weather_features.csv"
-    model_output_path = base / "models"/ "lgbm_flash_storm.txt"
+    model_output_path = base / "models"/ "lgbm_heavy_rain.txt"
 
     train(processed_path, model_output_path)
